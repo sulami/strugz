37e55bdbb4c52d9131d78ea7e55291ee3fe92dee
Robin Schroer <sulamiification@gmail.com>
2014-05-22 16:49:26 +0200

    added initial user model extension

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from services.models import Service
 
-# Create your models here.
+class User(AbstractUser):
+    provider = models.BooleanField(default=False)
+    favourites = models.ManyToManyField(Service, related_name='favourites')
+
