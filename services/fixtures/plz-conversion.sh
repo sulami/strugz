9f401f861eb3d27fb56ed67883c823e4102f003a
Robin Schroer <sulamiification@gmail.com>
2014-05-22 16:09:53 +0200

    added empty prototype apps

---
+++ services/fixtures/plz-conversion.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+tail -n +2 PLZ.tab | awk -F"\t" '{print "- model: master.location\n  pk: "FNR"\n  fields:\n    plz: "$2"\n    lon: "$3"\n    lat: "$4"\n    name: "$5}' > initial_data.yaml
+
