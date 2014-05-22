a7a6fc23e8605d1ef46d2a66f6456b94b187aa2e
Robin Schroer <sulamiification@gmail.com>
2014-05-22 16:49:48 +0200

    added service and location models, updated fixtures

--- services/models.py
+++ services/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    plz = models.IntegerField()
+    lon = models.FloatField()
+    lat = models.FloatField()
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return str(self.plz) + ' - ' + self.name
+
+class Service(models.Model):
+    pass
+
