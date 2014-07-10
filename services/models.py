d91ee7e02c3851daadcc11bf8d616fc5341a5285
Robin Schroer <sulamiification@gmail.com>
2014-07-10 15:30:54 +0200

    services/models: imported model structure from prototype

--- services/models.py
+++ services/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.conf import settings
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
 
 class Location(models.Model):
     plz = models.IntegerField()
@@ -10,5 +17,33 @@ class Location(models.Model):
         return str(self.plz) + ' - ' + self.name
 
 class Service(models.Model):
-    pass
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=50)
+    desc = models.TextField(blank=True)
+    tel = models.CharField(max_length=15)
+    website = models.URLField(blank=True)
+    plz = models.IntegerField()
+    lon = models.FloatField()
+    lat = models.FloatField()
 
+    def __unicode__(self):
+        return self.name
+
+    def average_rating(self):
+        try:
+            l = []
+            for r in Rating.objects.filter(of=self):
+                l += [r.stars]
+            return round(sum(l)/float(len(l)), 2)
+        except:
+            return 0
+
+class Rating(models.Model):
+    by = models.ForeignKey(settings.AUTH_USER_MODEL)
+    of = models.ForeignKey(Service)
+    at = models.DateTimeField(auto_now=True)
+    stars = models.IntegerField()
+    text = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.by.username + '  - ' + self.of.name
