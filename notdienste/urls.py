e58606f7c3b928a57f221fb0f7fb9cce455e87bf
Robin Schroer <sulamiification@gmail.com>
2014-07-10 19:49:22 +0200

    notdienste/urls: added account-specific url mappings

--- notdienste/urls.py
+++ notdienste/urls.py
@@ -5,6 +5,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^account/', include('registration.backends.default.urls')),
+    url(r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html',
+        'redirect_field_name': 'redirect'},
+        name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/'},
+        name='logout'),
     url(r'', include('renderer.urls', namespace='renderer')),
 )
 
