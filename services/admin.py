0e316106c107ee7cdb5112fef7c7e61a1833b25c
Robin Schroer <sulamiification@gmail.com>
2014-07-10 19:57:04 +0200

    services/admin: added service-related models to admin interface

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from services.models import Category, Location, Service, Rating
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Service)
+admin.site.register(Rating)
+
