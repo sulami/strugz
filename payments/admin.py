b2fcc1558b9f127180e65ad8118c4f3199e5fdc5
Robin Schroer <sulami@peerwire.org>
2015-02-09 08:42:02 +0100

    add payment model

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Payment
+
+admin.site.register(Payment)
+
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+class Payment(models.Model):
+    amount = models.IntegerField()
+    date = models.DateField(auto_now=True)
+    user = models.ForeignKey(User)
+
