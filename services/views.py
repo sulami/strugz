1dec9873363402ced9f0612d9a2cbafcf2b57ef8
Robin Schroer <sulami@peerwire.org>
2015-03-24 01:07:14 +0100

    some more cleanup in views

--- services/views.py
+++ services/views.py
@@ -16,7 +16,6 @@ def index(request):
 
     return render(request, 'index.html', context)
 
-# Standorteingabe
 def search(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
     context = {
@@ -25,9 +24,7 @@ def search(request, category_id):
 
     return render(request, 'services/search.html', context)
 
-# Ausgabe der Suchergebnisse einer Branche nach Naehe zu Standort
 def category(request, category_id):
-    # Daten aufnehmen
     category = get_object_or_404(Category, pk=category_id)
 
     try:
@@ -37,7 +34,6 @@ def category(request, category_id):
         context = {
             'error': UNKNOWN_PLZ.format(SUPPORT_EMAIL)
         }
-
         return render(request, 'error.html', context)
 
     services = Service.objects.filter(category=category)
@@ -50,7 +46,6 @@ def category(request, category_id):
     for service in distances:
         slist += [(distances[service], service)]
     slist.sort()
-
     servicelist = paginate(slist, 10)
 
     context = {
@@ -62,11 +57,10 @@ def category(request, category_id):
 
     return render(request, 'services/category.html', context)
 
-# Seite eines Service-Providers
 def service(request, service_id):
     service = get_object_or_404(Service, pk=service_id)
     context = {}
-    # Kontaktdaten wurden abgerufen, Captcha auslesen
+    # Contact data was requested, check the captcha.
     # context['contact'] = 0
     # if request.method == 'POST':
     #     form = CaptchaForm(request.POST)
@@ -75,28 +69,24 @@ def service(request, service_id):
     # else:
     #     form = CaptchaForm()
 
-    # Nutzer ist eingeloggt, hole bestehende Bewertung falls vorhanden
     if request.user.is_authenticated:
         try:
-            r = Rating.objects.get(by=request.user,of=service)
+            r = Rating.objects.get(by=request.user, of=service)
             context['rating'] = r
-        except:
-            pass # FIXME WTF?
-        # Bewertung wurde abgegeben/modifiziert
+        except: # There is no past rating of this user for this service.
+            pass
         if 'r' in request.GET:
+            rs = int(request.GET['r']
             try:
-                r.stars = int(request.GET['r'])
+                r.stars = rs
             except:
-                r = Rating(by=request.user,of=service,stars=int(request.GET['r']))
+                r = Rating(by=request.user, of=service, stars=rs)
             r.text = request.GET['rt']
-            if 6 > int(r.stars) > 0:
+            if 6 > rs > 0:
                 r.save()
             context['rating'] = r
 
-    # Ratings holen
-    ratinglist = Rating.objects.filter(of=service).order_by('-at')
-
-    ratings = paginate(ratinglist, 5)
+    ratings = paginate(Rating.objects.filter(of=service).order_by('-at'), 5)
 
     context['service'] = service
     context['ratings'] = ratings
@@ -105,8 +95,8 @@ def service(request, service_id):
     return render(request, 'services/service.html', context)
 
 def manage(request):
-    if not request.user.is_authenticated: # FIXME
-        return redirect(index)
+    if not request.user.is_authenticated:
+        return redirect('/')
 
     return render(request, 'services/manage.html')
 
