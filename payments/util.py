97b801869be9b0245385858cb7789c804b17478d
Robin Schroer <sulami@peerwire.org>
2015-03-30 18:51:25 +0200

    add some templates and error handling for the payment process

--- payments/util.py
+++ payments/util.py
@@ -1,9 +1,15 @@
+from braintree.error_result import ErrorResult
+
 from notdienste.settings import PAYMENT_BACKEND
 
 def create_payment(user, nonce):
     """Create a payment and return success (hopefully)"""
 
     pm = PAYMENT_BACKEND.create_payment_method(user, nonce)
+
+    if isinstance(pm, ErrorResult):
+        return False
+
     tr = PAYMENT_BACKEND.create_transaction('10.00', pm.payment_method.token)
     result = PAYMENT_BACKEND.submit_settlement(tr.transaction.id)
 
